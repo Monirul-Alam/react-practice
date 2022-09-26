c424d3ba115c662a87eb13213094013336c8de3c
Monirul Alam <monirulcr2325@gmail.com>
2022-09-26 18:53:38 +0600

    learing setState function

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,25 +1,36 @@
+import { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class  App extends Component {
+  constructor() {
+    super();
+    
+    this.state = {
+      name: {firstname: 'Yihua', lastName : 'Zhang'}, 
+      company : 'meta'
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+         <p>Hi {this.state.name.firstname} {this.state.name.lastName}, I work at {this.state.company} </p>
+         <button 
+         onClick={() => {
+          this.setState({ name : {firstname:'Monirul', lastName: 'Alam' }
+          
+          });
+          console.log(this.state);
+         }}> change it</button>
+        </header>
+      </div>
+    );
+
+  }
+  
 }
 
 export default App;
